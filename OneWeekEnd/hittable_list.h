b8b06e6dd8817e13c51c5397fe673afc849e4761
Tony HENG <tony.heng@epita.fr>
2020-09-20 15:42:50 +0200

    Add BVH optimization

--- OneWeekEnd/hittable_list.h
+++ OneWeekEnd/hittable_list.h
@@ -2,6 +2,7 @@
 #define HITTABLE_LIST_H
 
 #include "hittable.h"
+#include "aabb.h"
 
 #include <memory>
 #include <vector>
@@ -19,7 +20,8 @@ class hittable_list : public hittable {
 
         virtual bool hit(
             const ray& r, double tmin, double tmax, hit_record& rec) const override;
-
+        
+        virtual bool bounding_box(double t0, double t1, aabb& output_box) const override;
     public:
         std::vector<shared_ptr<hittable>> objects;
 };
@@ -38,6 +40,21 @@ bool hittable_list::hit(const ray& r, double t_min, double t_max, hit_record& re
     }
 
     return hit_anything;
+}
+
+bool hittable_list::bounding_box(double t0, double t1, aabb& output_box) const {
+    if (objects.empty()) return false;
+
+    aabb temp_box;
+    bool first_box = true;
+
+    for (const auto& object : objects) {
+        if (!object->bounding_box(t0, t1, temp_box)) return false;
+        output_box = first_box ? temp_box : surrounding_box(output_box, temp_box);
+        first_box = false;
+    }
+
+    return true;
 }
 
 #endif
--- OneWeekEnd/sphere.h
+++ OneWeekEnd/sphere.h
@@ -13,6 +13,8 @@ class sphere : public hittable {
         virtual bool hit(
             const ray& r, double tmin, double tmax, hit_record& rec) const override;
 
+        virtual bool bounding_box(double t0, double t1, aabb& output_box) const override;
+
     public:
         point3 center;
         double radius;
@@ -50,5 +52,11 @@ bool sphere::hit(const ray& r, double t_min, double t_max, hit_record& rec) cons
     return false;
 }
 
+bool sphere::bounding_box(double t0, double t1, aabb& output_box) const {
+    output_box = aabb(
+        center - vec3(radius, radius, radius),
+        center + vec3(radius, radius, radius));
+    return true;
+}
 
 #endif
